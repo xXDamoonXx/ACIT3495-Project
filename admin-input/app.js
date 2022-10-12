9b3dd45e26b02043efbe79957b24539a7e931f0e
mrkeyboard <matthewdandar@gmail.com>
2022-10-12 08:45:16 -0700

    quick update

--- admin-input/app.js
+++ admin-input/app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 const app = express()
-const port = 3002
+const port = 3003
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
@@ -32,27 +32,16 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// const mysql = require("mysql");
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "Password123",
-//     database: "weather",
-//     port: 49155
-// });
-// connection.connect();
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Password123",
+    database: "weather",
+    port: 49155
+});
+connection.connect();
 
-// const insert_data = (data) => {
-//     MongoClient.connect(url, function(err, db) {
-//         if (err) throw err;
-//         var dbo = db.db("mydb");
-//         dbo.createCollection("weather", function(err, res) {
-//         if (err) throw err;
-//         console.log("Collection created!");
-//         db.close();
-//         });
-//     });
-// }
 const create_db = () => {
     var url = "mongodb://localhost:27017/mydb";
 
